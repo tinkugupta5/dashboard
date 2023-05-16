ce9f0b17c6ac7c5c576e966ef7a0d7c90b29faac
tinkugupta5 <tinku.gupta4@gmail.com>
2023-05-16 23:42:58 +0530

    adding arrow

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
         <Routes path='/'>
           <Route index element={<Home />} />
